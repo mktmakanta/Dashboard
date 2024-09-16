94e210ba6472a860765e3a0d19c70ba48267b870
mktmakanta <mktmakantamkt@gmail.com>
2024-09-17 00:48:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <Input />
+      <label htmlFor="ttmtmm">just</label>
+      <Input />
+    </>
+  );
+}
+
+export default App;
